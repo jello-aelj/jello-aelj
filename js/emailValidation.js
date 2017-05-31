e6b3d7c78985b08ce6f6903b0190a5cd01f16af6
Alexandre Stelzig <Alexandre.Stelzig@gmail.com>
2017-05-30 23:04:18 -0400

    attempt to fix crash

--- js/emailValidation.js
+++ js/emailValidation.js
@@ -74,7 +74,7 @@ function submitEmail(){
                 _replyto:email.val(),
                  email:email.val(),
                 comments:comments.val(),
-                Japaneselevel:level.val(),
+                Japaneselevel:level.options[levelSelector.selectedIndex].value,
                 _subject:'JELLO-AEJL REGISTRATION REQUEST (from website)'},
             dataType:"json",
             success:function() {
