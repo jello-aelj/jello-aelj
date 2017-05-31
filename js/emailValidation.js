7365e3c0f3b5426685ad8732712edae8defb5a8f
Alexandre Stelzig <Alexandre.Stelzig@gmail.com>
2017-05-30 22:48:52 -0400

    added feew things and email validation

--- js/emailValidation.js
+++ js/emailValidation.js
@@ -4,25 +4,82 @@
 // $('#someForm').on('submit', function(e) {
 //         e.preventDefault();
 
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
 function submitEmail(){
         
         //get the name field value
-        var name = $('#name').val();
+        var name = $('#name');
         //get the name field value
-        var email = $('#email').val();
+        var email = $('#email');
         //get the comments
-        var comments = $('#comments').val();
+        var comments = $('#comments');
                     
-        //pretend we don't need validation
+        var levelSelector = document.getElementById("level");
+        var level = levelSelector.options[levelSelector.selectedIndex].value;
+
+        //validation
+
+        var nameEmpty = name.val().trim() == "";
+        var emailEmpty = email.val().trim() == "";
+        var levelEmpty = level == "";
         
+        if(nameEmpty || emailEmpty || levelEmpty){
+
+            if(nameEmpty){
+                console.log("name empty");
+                document.getElementById("name").style.borderColor = "red";
+                document.getElementById("nameError").style.display = "unset";
+            }else{
+                document.getElementById("name").style.borderColor = "#CCCCCC";
+                document.getElementById("nameError").style.display = "none";
+            }
+
+            if(emailEmpty){
+                console.log("email empty");
+                document.getElementById("email").style.borderColor = "red";
+                document.getElementById("emailError").style.display = "unset";
+            }else{
+                document.getElementById("email").style.borderColor = "#CCCCCC";
+                document.getElementById("emailError").style.display = "none";
+            }
+
+            if(levelEmpty){
+                console.log("level empty");
+                document.getElementById("level").style.borderColor = "red";
+                document.getElementById("levelError").style.display = "unset";
+            }else{
+                document.getElementById("level").style.borderColor = "#CCCCCC";
+                document.getElementById("levelError").style.display = "none";
+            }
+
+            console.log('failed'); 
+        }else{
+            document.getElementById("name").style.borderColor = "#CCCCCC";
+            document.getElementById("nameError").style.display = "none";
+            document.getElementById("email").style.borderColor = "#CCCCCC";
+            document.getElementById("emailError").style.display = "none";
+            document.getElementById("level").style.borderColor = "#CCCCCC";
+            document.getElementById("levelError").style.display = "none";
+            console.log('success'); 
+        }
+
+
         //send to formspree
         $.ajax({
             url:'https://formspree.io/alex.stelzig@hotmail.fr',
             method:'POST',
-            data: {message: "hello!"},
+            data: {name:name,
+                _replyto:email,
+                 email:email,
+                comments:comments,
+                Japaneselevel:level,
+                _subject:'My Form Submission'},
             dataType:"json",
             success:function() {
-                console.log('success'); 
                 // $('#formBlock').hide();
                 // $('#thankyouBlock').show();
             }   
